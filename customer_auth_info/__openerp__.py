a51bf52d21fdb9adc93fe3979ddf7ea8d582a1f1
satya <satyay1990@gmail.com>
2016-01-11 00:24:34 +0530

    customer

---
+++ customer_auth_info/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Customer Authentication Information',
+    'version' : '1.1',
+    'author' : 'DEV Soft.',
+    'category': 'Sales',
+    'sequence': 10,
+    'summary': 'This module help us to store multiple authentication information for particular customer.',
+    'description': """
+   This module help us to store multiple authentication information of a particular customer.
+
+
+""",
+    'website': '',
+    'depends' : ['sale'],
+    'data': ["views/customer_auth_info.xml"],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'price': 25.99,
+    'currency': 'EUR',
+}
+
+
