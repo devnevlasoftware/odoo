cbe031779894b43beaeb396bc19c77d7be2cb739
satya <satyay1990@gmail.com>
2016-01-17 22:51:30 +0530

    update pro

---
+++ customer_auth_info/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#This will extend
+import models
+
+
---
+++ customer_auth_info/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Customer Authentication Information',
+    'version' : '1.1',
+    'author' : 'DEVN',
+    'category': 'Sales',
+    'sequence': 10,
+    'summary': 'This module store multiple authentication informations for particular customer.',
+    'description': """
+   This module store multiple authentication informations for particular customer.
+
+
+""",
+    'website': '',
+    'depends' : ['sale'],
+    'data': ["views/customer_auth_info.xml"],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'price': 25.99,
+    'currency': 'EUR',
+}
+
+
